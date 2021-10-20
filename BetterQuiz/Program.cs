046ab38b0313550e204df7a5f4c4881871ea93da
Samuel Palmér <samiskramis@gmail.com>
2021-10-20 13:30:30 +0200

    fix stuff in loop

--- BetterQuiz/Program.cs
+++ BetterQuiz/Program.cs
@@ -8,6 +8,11 @@ namespace BetterQuiz
     {
         static void Main(string[] args)
         {
+            StartQuiz();
+            Console.WriteLine("done man");
+            Console.ReadLine();
+
+
             //Hälsa på användaren
             GreetUser();
             //Klar med att häsla på användaren
@@ -76,15 +81,37 @@ namespace BetterQuiz
             Den andra dimensionen är allt den innehåller, alltså [0] är frågan, [1] rätt svar, [2] och [3] är alternativen
             */
 
-            List<int> questions = new List<int>();
+            //Välj random questions som sen kan användas till quizet när man spelar det
+            List<int> questionsToUse = new List<int>();
+            Random generator = new Random();
+
             for (int i = 0; i < 5; i++)
             {
-                Random generator = new Random();
-
                 int pick = generator.Next(output.Length);
-                while (questions.Contains(pick))
+                while (questionsToUse.Contains(pick))
                 {
                     pick = generator.Next(output.Length);
+                }
+                questionsToUse.Add(pick);
+            }
+            //Det finns nu 5 olika nummer, som ska vara motsvarande till frågorna i qna.txt
+
+            //Ladda in svar, alternativ och shuffle'a dem i en arr. Sen ta svaret och se till så att det är rätt eller fel
+            foreach (int q in questionsToUse)
+            {
+                List<string> unsortedAnswers = new List<string> { questionArr[q][1], questionArr[q][2], questionArr[q][3] };
+                List<string> sortedAnswers = new List<string>();
+
+                for (int i = 3; i > 0; i--)
+                {
+                    int temp = generator.Next(0, i);
+                    sortedAnswers.Add(unsortedAnswers[temp]);
+                    unsortedAnswers.RemoveAt(temp);
+                }
+
+                foreach (string item in sortedAnswers)
+                {
+                    Console.WriteLine(item);
                 }
             }
         }
