a47d25e294cc88c5645f9a014f1b29b369acf500
Samuel Palmér <samiskramis@gmail.com>
2021-10-21 23:12:32 +0200

    Add Question Feature left

--- BetterQuiz/Program.cs
+++ BetterQuiz/Program.cs
@@ -97,6 +97,7 @@ namespace BetterQuiz
             //Det finns nu 5 olika nummer, som ska vara motsvarande till frågorna i qna.txt
 
             //Ladda in svar, alternativ och shuffle'a dem i en arr. Sen ta svaret och se till så att det är rätt eller fel
+            int points = 0;
             foreach (int q in questionsToUse)
             {
                 List<string> unsortedAnswers = new List<string> { questionArr[q][1], questionArr[q][2], questionArr[q][3] };
@@ -104,16 +105,82 @@ namespace BetterQuiz
 
                 for (int i = 3; i > 0; i--)
                 {
-                    int temp = generator.Next(0, i);
+                    int max = unsortedAnswers.Count;
+                    int temp = generator.Next(0, max);
                     sortedAnswers.Add(unsortedAnswers[temp]);
                     unsortedAnswers.RemoveAt(temp);
                 }
 
-                foreach (string item in sortedAnswers)
+                //Frågar användaren frågan och visar alla svar
+                Console.Clear();
+                Console.WriteLine(questionArr[q][0]);
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("(answer with number or text)");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"1) {sortedAnswers[0]}");
+                Console.WriteLine($"2) {sortedAnswers[1]}");
+                Console.WriteLine($"3) {sortedAnswers[2]}");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                //Tar svaret och nu testar om det går att converta, om det går så kommer vi göra det och ge svar
+                int intSvar;
+                string stringSvar = Console.ReadLine();
+                bool canConvert = Int32.TryParse(stringSvar, out intSvar);
+
+                //Låt användaren se sitt svar i 0.5 sekunder, sen cleara så att det ser bra ut och visa resultat
+                System.Threading.Thread.Sleep(500);
+                Console.Clear();
+
+                //Boolen canConvert är om det går att göra det till en int, annars testar den om man skrev svaret
+                if (canConvert)
                 {
-                    Console.WriteLine(item);
+                    if (intSvar >= 1 && intSvar <= 3)
+                    {
+                        if (sortedAnswers[intSvar - 1] == questionArr[q][1])
+                        {
+                            Console.WriteLine("Correct, 1 point awarded!");
+                            points++;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong, no points awarded!");
+                    }
                 }
+                else if (stringSvar.ToLower() == questionArr[q][1].ToLower())
+                {
+                    Console.WriteLine("Correct, 1 point awarded!");
+                    points++;
+                }
+                else
+                {
+                    Console.WriteLine($"Can't convert '{stringSvar}' to an answer.");
+                }
+                System.Threading.Thread.Sleep(1000);
             }
+
+            //Ge användaren olika svar beroende på hur bra svar man fick
+            if (points <= 0)
+            {
+                Console.WriteLine($"You got {points}/5 points! Time to study more!");
+            }
+            else if (points <= 3)
+            {
+                Console.WriteLine($"You got {points}/5 points! About halfway there!");
+            }
+            else if (points == 4)
+            {
+                Console.WriteLine("You are as close to perfect as possible. 4/5 points!");
+            }
+            else
+            {
+                Console.WriteLine("Wow, you did a great job. 5/5, not bad");
+            }
+        }
+
+        private static void AddQuestion()
+        {
+
         }
     }
 }
